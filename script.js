a67306ce5270e8f4a2cd510cade7429078423b09
ttigishvili <ttigishvili@gmail.com>
2022-07-02 11:26:49 +0400

    dasasrulebelia me-6 davaleba

---
+++ script.js
@@ -0,0 +1,77 @@
+
+                                    // davaleba 1   ( DONE )
+
+// let names = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas'] ;
+// console.log(names[0]);
+// console.log(names[1]);             
+// console.log(names[5]);
+// console.log(names[6]);
+
+
+                            // davaleba 2   DONE
+
+
+
+// let number = [32, 10, 'hello', "null", 'hello2', 50 ];
+
+// for (let item of number) {
+    
+//     if (item % 5 == 0 ){
+//         console.log(item);
+//     }                      
+     
+// }
+
+
+
+                                //    davaleba 3   DONE
+
+
+
+
+// let numbers = [ 5, 9, 11, 25, 17, 7, 4, 100, 28, 36, 89, 129, 3, 22, ];
+// for (let item of numbers){
+//     if (item > 5){
+//         console.log(item);
+//     }
+// }
+
+
+                                // davaleba 4   DONE
+
+
+
+//  let  user = {
+//     firstname: 'giorgi',
+//     lastname: 'smith',
+//      age: 25,
+//    studentstatus: 'active',
+//    };
+//    console.log(user.studentstatus)                               
+
+
+
+                                // davaleba 5   DONE
+
+
+
+
+// let names = ['aleko', 'nika', 'tika', 'pika'];
+
+// for ( let x = 0; x < names.length; x++) {                        
+//         console.log(names[x]);
+//     }
+
+
+
+
+                            //    davaleba 6
+
+// let user = {
+// 	name: ‘giorgi’,                                              
+// 	age:  20,
+// 	studentstatus: active
+// }
+
+
+   
